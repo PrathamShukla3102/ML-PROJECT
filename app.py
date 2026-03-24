aeba01ce40bfd09b8518c58dc3d159c9a255990a
PrathamShukla3102 <162474038+PrathamShukla3102@users.noreply.github.com>
2026-03-24 14:53:59 +0530

    best model trained

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from src.mlproject.components.data_ingestion import DataIngestion
 from src.mlproject.components.data_ingestion import DataIngestionConfig
 from src.mlproject.components.data_transformation import DataTransformationConfig,DataTransformation
 import sys
+from src.mlproject.components.model_tranier import ModelTrainerConfig,ModelTrainer
+
 
 
 if __name__=="__main__":
@@ -17,7 +19,12 @@ if __name__=="__main__":
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
         train_arr,test_arr,_=data_transformation.initiate_data_transormation(train_data_path,test_data_path)
+        
+        ## Model Training
 
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+        
     except Exception as e:
         logging.info("Custom Exception")
         raise CustomException(e,sys)
